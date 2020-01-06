4f0b88eca3d0f35f413de3b9fe00e1479bebd40e
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-01-06 14:48:04 +0100

    [ADD] struct in my.h

--- include/my.h
+++ include/my.h
@@ -13,6 +13,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct coef
+{
+    float a0;
+    float a1;
+    float a2;
+    float a3;
+    float a4;
+} coef_t;
+
 int torus(int ac, char **av);
 void display_help(void);
 int error_handling(int ac, char **av);
