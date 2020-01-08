3dd7d3872dcc43a90f05efe7a29b32d553256ef5
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-01-08 19:16:35 +0100

    [UDP] error_handling / change return [FIX] torus / fix seg fault

--- src/error_handling.c
+++ src/error_handling.c
@@ -16,17 +16,18 @@ void error(char *str)
 int verify_nbr(int ac, char **av)
 {
     int isfloat;
-    for (int n1 = 1; n1 != ac; n1++) {
+
+    for (int i = 1; i != ac; i++) {
         isfloat = 0;
-        for (int n2 = 0; av[n1][n2] != '\0'; n2++) {
-            if ((av[n1][n2] < '0' || av[n1][n2] > '9') && av[n1][n2] != '-' &&
-                av[n1][n2] != ',')
-                return (84);
-            if (av[n1][n2] == '-' && (av[n1][n2 + 1] == '\0' || n2 != 0))
-                return (84);
-            if (av[n1][n2] == ',' && (av[n1][n2 + 1] == '\0' || n2 == 0)) {
+        for (int j = 0; av[i][j] != '\0'; j++) {
+            if ((av[i][j] < '0' || av[i][j] > '9') && av[i][j] != '-' &&
+                av[i][j] != ',')
+                return (1);
+            if (av[i][j] == '-' && (av[i][j + 1] == '\0' || j != 0))
+                return (1);
+            if (av[i][j] == ',' && (av[i][j + 1] == '\0' || j == 0)) {
                 if (isfloat == 1)
-                    return(84);
+                    return(1);
                 isfloat = 1;
             }
         }
@@ -40,20 +41,20 @@ int error_handling(int ac, char **av)
         return (0);
     if (ac != 8) {
         error(ARGNBR);
-        return (84);
+        return (1);
     }
     if (verify_nbr(ac, av) == 84) {
         error(BADARG);
-        return(84);
+        return(1);
     }
     if ((av[1][0] != '1' && av[1][0] != '2' && av[1][0] != '3') ||
         av[1][1] != '\0') {
         error(BADOPT);
-        return (84);
+        return (1);
     }
     if (av[7][0] == '-') {
         error(BADN);
-        return (84);
+        return (1);
     }
     return(0);
 }
--- src/torus.c
+++ src/torus.c
@@ -9,11 +9,10 @@
 
 int torus(int ac, char **av)
 {
-    printf("n");
     float *cf = malloc(5 * sizeof(float));
-    coef_t coef;
-
-    if (error_handling(ac, av) == 84) {
+    coef_t *coef = malloc(5 * sizeof(float));
+    printf("HEY\n");
+    if (error_handling(ac, av) == 1) {
         free(cf);
         return (84);
     }
@@ -21,17 +20,19 @@ int torus(int ac, char **av)
         display_help();
         return (0);
     }
-    for (int i = 0; i != ac - 1; i++)
-        cf[i] = atof(av[i + 2]);
-    coef_t.a0 = cf[0];
-    coef_t.a1 = cf[1];
-    coef_t.a2 = cf[2];
-    coef_t.a3 = cf[3];
-    coef_t.a4 = cf[4];
-    printf("a0 = %f\n", coef.a0);
-    printf("a1 = %f\n", coef.a1);
-    printf("a2 = %f\n", coef.a2);
-    printf("a3 = %f\n", coef.a3);
-    printf("a4 = %f\n", coef.a4);
+    printf("HEY\n");
+    for (int i = 0, j = 2; j != ac - 1; i++, j++)
+        cf[i] = atof(av[j]);
+    printf("HEY\n");
+    coef->a0 = cf[0];
+    coef->a1 = cf[1];
+    coef->a2 = cf[2];
+    coef->a3 = cf[3];
+    coef->a4 = cf[4];
+    printf("a0 = %f\n", coef->a0);
+    printf("a1 = %f\n", coef->a1);
+    printf("a2 = %f\n", coef->a2);
+    printf("a3 = %f\n", coef->a3);
+    printf("a4 = %f\n", coef->a4);
     return (0);
 }
